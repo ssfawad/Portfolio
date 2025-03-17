cdc6a42b123bf7bb6165b07a0513a4397c15a84c
Anzhelika Kostyuk <akostyuk@conestogac.on.ca>
2025-03-17 12:57:54 -0400

    Added responsiveness and a typewriter effect

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -51,17 +51,22 @@ const Hero = () => {
   return (
     <>
       <section className="min-h-screen w-full flex flex-col relative items-center justify-center">
-        <div className="max-w-7xl mx-auto flex flex-col c-space gap-3 items-center">
-          <p className="sm:text-3xl text-2xl font-medium text-white text-center">
+        <div className="max-w-7xl mx-auto flex flex-col c-space gap-7 items-center">
+          <p className="md:text-5xl sm:text-4xl text-2xl font-medium text-white text-center">
             Hi, I am{" "}
             <LinearGradient gradient={["to left", "#ff9720 ,#fc0865"]}>
               Anzhelika
             </LinearGradient>
           </p>
-          <p className="text-center text-white xl:text-6xl md:text-5xl sm:text-4xl text-3xl font-generalsans font-black !leading-normal">
+          <p
+            className=" text-center text-white xl:text-6xl md:text-5xl sm:text-4xl text-3xl font-black !leading-normal relative w-[max-content]
+before:absolute before:inset-0 before:animate-typewriter before:bg-[#1a191e]
+after:absolute after:inset-0 after:w-[0.125em] after:animate-caret after:bg-white"
+          >
             Software Developer
           </p>
-          <p className="max-w-4xl text-center justify-center text-white">
+
+          <p className="max-w-4xl text-center justify-center text-white xl:text-xl md:text-lg sm:text-sm text-md">
             I'm a passionate software developer with expertise in web and game
             development using JavaScript, TypeScript, C#, and Unity. I build
             interactive experiences and web applications, leveraging both
@@ -70,7 +75,7 @@ const Hero = () => {
             innovative solutions that enhance user experiences.
           </p>
         </div>
-        <div className="flex flex-row gap-2 justify-center mt-5 max-w-7xl">
+        <div className="flex flex-row gap-2 justify-center mt-10 max-w-7xl">
           <SocialIcon
             className="heroIcon"
             url="https://www.instagram.com/a_akcio/?hl=en"
