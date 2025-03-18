67bf053e1fa351d4a5a8029cc3ce0844aceb7a6c
Anzhelika Kostyuk <akostyuk@conestogac.on.ca>
2025-03-18 16:33:20 -0400

    Updated the nav links.

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -11,11 +11,16 @@ export const navLinks = [
   },
   {
     id: 3,
+    name: "Skills",
+    href: "#skills",
+  },
+  {
+    id: 4,
     name: "Projects",
     href: "#projects",
   },
   {
-    id: 4,
+    id: 5,
     name: "Contact",
     href: "#contact",
   },
--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -46,7 +46,7 @@ const ExperienceCard = ({ experience }) => {
 const Experience = () => {
   return (
     <>
-      <section className="w-full flex justify-center mb-20">
+      <section className="w-full flex justify-center mb-20" id="experience">
         <div className="flex flex-col w-full max-w-7xl items-center justify-start">
           <div className="w-full">
             <p className="mb-10 xl:text-5xl md:text-4xl sm:text-3xl text-2xl font-black !leading-normal">
