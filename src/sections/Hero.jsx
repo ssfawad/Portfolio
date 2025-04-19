853bf4a8cd5bc36424e03cd43106a265cefe97c1
Anzhelika Kostyuk <akostyuk@conestogac.on.ca>
2025-04-19 19:45:03 -0400

    Updated headings

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -28,19 +28,19 @@ const Hero = () => {
         id="home"
       >
         <div className="max-w-7xl mx-auto flex flex-col c-space gap-7 items-center">
-          <p className="md:text-5xl sm:text-4xl text-2xl font-medium text-white text-center">
+          <h2 className="md:text-5xl sm:text-4xl text-2xl font-medium text-white text-center">
             Hi, I am{" "}
             <LinearGradient gradient={["to left", "#ff9720 ,#fc0865"]}>
               Anzhelika
             </LinearGradient>
-          </p>
-          <p
+          </h2>
+          <h2
             className=" text-center text-white xl:text-6xl md:text-5xl sm:text-4xl text-3xl font-black !leading-normal relative w-[max-content]
 before:absolute before:inset-0 before:animate-typewriter before:bg-[#1a191e]
 after:absolute after:inset-0 after:w-[0.125em] after:animate-caret after:bg-white"
           >
             Software Developer
-          </p>
+          </h2>
 
           <p className="max-w-4xl text-center justify-center text-white xl:text-xl md:text-lg sm:text-sm text-md">
             I&apos;m a passionate software developer with expertise in web and
