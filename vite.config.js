87406b0e687792078781078c4d6402e090a847c5
Syed Fawad <syedsfawad@gmail.com>
2025-04-30 22:19:06 -0500

    fixed issue with images not loading in prod build

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',              
+  base: '/Portfolio-Website/',
   plugins: [react()],
   build: {
-    outDir: 'docs',        
+    outDir: 'docs',
   },
 })
